16d58eadc90b83a69fb00595d5174b187aeda452
Jan Hümmelink <j.huemmelink@gmail.com>
2023-03-19 14:28:33 +0100

    final state to submit

--- task5.py
+++ task5.py
@@ -1,7 +1,7 @@
-"""
+'''
 # Task 5
 use the trained Model to let the user verify his tweets
-"""
+'''
 
 import streamlit as st
 import pandas as pd
@@ -14,77 +14,55 @@ from sklearn.model_selection import GridSearchCV
 from nltk.stem.porter import PorterStemmer
 from sklearn.model_selection import train_test_split
 import joblib
+from nltk.stem import WordNetLemmatizer
+
+st.title('Task 5')
 
 
-train = pd.read_csv("Data/train.csv")
-train = train.drop("id", axis=1)
+test = pd.read_csv('Data/test.csv')
+test = test.drop('id', axis=1)
 
 
-st.write("Let's have a look on the training data:")
-st.write(train)
+st.write('Let us have a look on the test data: ')
+st.write(test)
 
-stop = stopwords.words("english")
+stop = stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
 
 
 def cleaning_tweets(tweet: str):
-    tweet = tweet.replace("#", "")
-    tweet = tweet.replace("@user", "")
-    tweet = " ".join(tweet.split())
-    # searches for everything that is not a character between a and z, not a number and not a whitespace
-    tweet = re.sub(r'[^A-Za-z0-9\s]', '', tweet)
+    tweet = tweet.replace('@user', '')
+    tweet = ' '.join(tweet.split())
+    tweet = tweet.lower()
+    # searches for everything that is not a character between a and z, and not a whitespace
+    tweet = re.sub(r'[^a-z\s]', '', tweet)
 
     splitted_tweet = tweet.split()
     result = []
     for x in splitted_tweet:
         if x not in stop:
-            result.append(x)
+            result.append(lemmatizer.lemmatize(x))  # add the lemmatized word
 
-    return " ".join(result)
-
-
-train["tweet"] = train["tweet"].apply(cleaning_tweets)
-
-st.write("Now the tweets are cleaned:")
-st.write(train)
+    return ' '.join(result)
 
 
-# param stratify: keep the proportion of values in that set
-x_train, x_test, y_train, y_test = train_test_split(
-    train["tweet"], train["label"], test_size=0.3, stratify=train["label"])
+test['tweet_cleaned'] = test['tweet'].apply(cleaning_tweets)
 
+st.write('Now the tweets are cleaned:')
+st.write(test)
 
-porter = PorterStemmer()
+clf = joblib.load('model.pkl')
 
+st.write('now predict the values for the test values:')
+test['prediction'] = clf.predict(test['tweet_cleaned'])
+st.write(test)
 
-def tokenizer_porter(text):
-    return [porter.stem(word) for word in text.split()]
-
-
-tfidf = TfidfVectorizer(strip_accents=None,
-                        lowercase=True,
-                        preprocessor=None,
-                        token_pattern=None)
-
-
-param_grid = [{'vect__ngram_range': [(1, 1)],
-               'vect__tokenizer': [tokenizer_porter],
-               'clf__penalty': ['l1'],
-               'clf__C': [10.0]},
-              ]
-
-
-clf = joblib.load("model.pkl")
-
-st.write("now predict the values for the test values:")
-predictions = clf.predict(x_test)
-df1 = pd.DataFrame({"tweets": x_test, "labels": predictions})
-st.write(df1)
-
-st.text_input("test your own tweet:", key="own_tweet")
-st.write("cleaned input: "+cleaning_tweets(st.session_state.own_tweet))
+st.header('Try your own tweets!')
+st.text_input('test your own tweet:', key='own_tweet')
+st.write('cleaned input: '+cleaning_tweets(st.session_state.own_tweet))
 if st.session_state.own_tweet:
     result = clf.predict([cleaning_tweets(st.session_state.own_tweet)])
     if result == [1]:
-        st.write("your tweet seems to contain hate speech!")
+        st.write('your tweet seems to contain hate speech!')
     else:
-        st.write("your tweet seems to contain no hate speech!")
+        st.write('your tweet seems to contain no hate speech!')
